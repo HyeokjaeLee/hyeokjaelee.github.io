2816e172676c7d9eb4491fbc9e9ab4cfdfe20a1a
Hyeokjae Lee (Nagle) <71566740+HyeokjaeLee@users.noreply.github.com>
2021-04-15 19:19:20 +0900

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ import Head from "next/head";
 import { Footer } from "../component/layout/Footer";
 import styles from "../styles/Home.module.css";
 import { TobBar, BottomBar } from "../component/layout/PageForm";
-import { Button } from "reactstrap";
-import {MainCard} from "../component/layout/MainCard"
+import { Button, Progress, Jumbotron } from "reactstrap";
+import { MainCard } from "../component/layout/MainCard";
 
 export default function Home() {
   return (
@@ -17,11 +17,33 @@ export default function Home() {
       </Head>
       <TobBar />
       <main className={styles.main}>
-        <span className={styles.test}>
-          <h1 className={styles.texttest}><span style={{color:"#A892DD"}}>const </span>lazy_developer<span style={{color:"#89DDFF"}}>=</span><br/><span style={{color:"#B9E876"}}>"이혁재입니다."</span></h1>
-         {/*<div className={styles.test3}><MainCard/></div>*/}
-          <Button outline color="primary" className={styles.test2}><h2>▶</h2></Button>
-        </span>
+        <Jumbotron className={styles.title_container}>
+          <span className={styles.title_container}>
+            <h2 className={styles.title_text}>
+              <span style={{ color: "#A892DD" }}>const </span>lazy_developer
+              <span style={{ color: "#89DDFF" }}>=</span>
+              <br/>
+              <span style={{ color: "#B9E876" }}>"이혁재입니다."</span>
+            </h2>
+            <hr/>
+            {/*<div className={styles.test3}><MainCard/></div>*/}
+            <div className={styles.title_button}>
+              Heroku 서버를 깨우는 중입니다.<br/>
+              잠시만 기다려주세요.
+            <Progress multi style={{ width: "60%", marginTop: "20px"}}>
+              <Progress bar animated value="15">
+                Stripes
+              </Progress>
+              <Progress bar animated color="success" value="30">
+                Animated Stripes
+              </Progress>
+            </Progress>
+              {/*<Button outline color="primary" className={"back face"}>
+                <h2>▶</h2>
+              </Button>*/}
+            </div>
+          </span>
+        </Jumbotron>
       </main>
       <footer className={styles.footer}>
         <BottomBar />
