94528172234cafb5131629dc7422cc9583ce5ce1
Hyeokjae Lee (Nagle) <71566740+HyeokjaeLee@users.noreply.github.com>
2021-05-12 19:52:47 +0900

    ver 0.1.2
    Port old code

---
+++ component/layout/Page.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import styles from "../../styles/Page.module.css";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
+
+export const Page = (props) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{props.title}</title>
+        <link rel="icon" href="/img/Logo.png" />
+      </Head>
+      <TobBar />
+      <main className={styles.main}>{props.children}</main>
+
+      <Footer />
+    </div>
+  );
+};
+
+const Footer = (props) => {
+  return (
+    <footer className={styles.footer}>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "1em" }}>
+        <a href="https://www.instagram.com/2z_4_me/" target="_blank">
+          <img src="https://img.shields.io/badge/Instagram-F1354D?style=flat-square&logo=Instagram&logoColor=white" />
+        </a>
+        <a href="https://github.com/HyeokjaeLee" target="_blank">
+          <img src="https://img.shields.io/badge/Github-3B3B3B?style=flat-square&logo=Github&logoColor=white" />
+        </a>
+        <a href="mailto:leehyeokjae97@gmail.com" target="_blank">
+          <img src="https://img.shields.io/badge/Gmail-BE293C?style=flat-square&logo=Gmail&logoColor=white" />
+        </a>
+      </div>
+      <div
+        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+      >
+        <img src="/img/Logo.png" alt="Logo" className={styles.logo} />
+        <span style={{ color: "#d7c49e", fontSize: "15px" }}>
+          © All rights reserved.
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+const TobBar = (props) => {
+  const [collapsed, setCollapsed] = useState(true);
+  const toggleNavbar = () => setCollapsed(!collapsed);
+  return (
+    <div style={{ paddingBottom: "1%", width: "100%" }}>
+      <Navbar light={true} full={true} style={{ backgroundColor: "#F0EDCC" }}>
+        <NavbarBrand
+          href="/"
+          className="mr-auto"
+          style={{ fontSize: "3vh", color: "#02343F" }}
+        >
+          {"<Nagle/>"}
+        </NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="/posts/">Posts</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">Demo Projects</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/HyeokjaeLee">GitHub</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,52 @@
-import Link from 'next/link'
+// pages/index.tsx
+
+import React, { useState } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { Progress } from "reactstrap";
+import { Page } from "../component/layout/Page";
 export default function Home() {
   return (
-    <div>
-      Hello World.{' '}
-      <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
-        <a>About</a>
-      </Link>
-    </div>
-  )
+    <Page title="Nagle">
+      <div className={styles.title_container}>
+        <div className={styles.main_title}>
+          <div className={styles.test} />
+          <span>
+            Lazy Developer
+            <br />
+            이혁재입니다.
+          </span>
+        </div>
+        <div className={styles.sub_title}>
+          <Progress
+            multi
+            style={{ width: "50%", height: "33%", borderRadius: "100px" }}
+          >
+            <Progress
+              bar
+              animated
+              value="70"
+              style={{ backgroundColor: "#343148" }}
+            ></Progress>
+          </Progress>
+        </div>
+      </div>
+    </Page>
+  );
+}
+
+{
+  /*      <div className={styles.title_container}>
+        <div className={styles.main_title}>
+          <h1>
+            Lazy Developer
+            <br />
+            이혁재 입니다.
+          </h1>
+          heroku 서버를 켜는 중입니다.
+        </div>
+        <div className={styles.sub_title}>
+         
+        </div>
+      </div>*/
 }
