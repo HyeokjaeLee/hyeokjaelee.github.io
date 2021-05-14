df951364b4ef2e97c1a41a984801da3f3d937bcd
Hyeokjae Lee (Nagle) <71566740+HyeokjaeLee@users.noreply.github.com>
2021-05-14 22:14:30 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,32 +2,23 @@
 
 import React, { useState } from "react";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { Progress } from "reactstrap";
-import { Page } from "../components/layout/Page";
+import styles from "../styles/about.module.css";
 import { TobBar } from "../components/common/Topbar";
-export default function Home() {
+export default function About() {
   return (
-    <div className={styles.body}>
-      <div style={{ height: "150vh" }}>
-        <TobBar />
+    <div>
+      <TobBar />
+      <div className={styles.color}>
+        <span className={styles.text}>
+          HYEOKJAE LEE
+          <h1>
+            I'm Lazy
+            <br />
+            Developer;
+          </h1>
+        </span>
+        <div className={styles.dev_img} />
       </div>
     </div>
   );
-}
-
-{
-  /*      <div className={styles.title_container}>
-        <div className={styles.main_title}>
-          <h1>
-            Lazy Developer
-            <br />
-            이혁재 입니다.
-          </h1>
-          heroku 서버를 켜는 중입니다.
-        </div>
-        <div className={styles.sub_title}>
-         
-        </div>
-      </div>*/
 }
