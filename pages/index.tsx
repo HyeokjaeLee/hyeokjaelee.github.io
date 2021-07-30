bd82ffeb40ec46dc81d84056c2e137e48a8df1c7
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-07-30 09:04:33 +0900

    :newspaper: Create: Base files 생성

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+export default function Home() {
+  return (
+    <div className="container">
+      <div>
+        <span>Next.js+Typescript+gh-pages</span>
+      </div>
+    </div>
+  );
+}
