39726051f9e86e6b7527873249c0a9fc0b212a3b
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-07-30 09:16:49 +0900

    Deploy Next.js to gh-pages

---
+++ out/_next/static/YGXnuOsWTY39rAkYXfAw4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c69336f526fd88c00dc8.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
