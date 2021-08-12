fc44e7177e6da7b5d6433b25052bfa96d4217466
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-08-12 20:26:00 +0900

    :sparkles: Feat: 다크모드 추가

--- src/templates/layout.tsx
+++ src/templates/layout.tsx
@@ -7,19 +7,33 @@ import "../assets/sass/global.scss";
 
 import { Header } from "../components/header";
 import { Footer } from "../components/footer";
-const TemplateWrapper = ({ children, location }: any) => (
-  <>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: `description`, content: `Sample` },
-        { name: `keywords`, content: `sample, something` },
-      ]}
-    />
-    <Header />
-    <Main location={location}>{children}</Main>
-    <Footer />
-  </>
-);
+const TemplateWrapper = ({ children, location }: any) => {
+  const [mode, setMode] = useState("light");
+  const click = () => setMode(mode === "light" ? "dark" : "light");
+  return (
+    <>
+      <Helmet
+        title="Gatsby Default Starter"
+        meta={[
+          { name: `description`, content: `Sample` },
+          { name: `keywords`, content: `sample, something` },
+        ]}
+        bodyAttributes={{ class: mode }}
+      />
+      <button
+        className="switch"
+        onClick={() => {
+          click();
+        }}
+      >
+        <div className={mode} />
+      </button>
+
+      <Header />
+      <Main location={location}>{children}</Main>
+      <Footer />
+    </>
+  );
+};
 
 export default TemplateWrapper;
