7d1d5199e693499c4050bb16088624076a4b633b
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-10-28 10:33:45 +0900

    🔥 refactor: remove comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
       resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
-          include: /assets/.svg$, // See below to configure properly
+          include: /assets/.svg$,
         },
       },
     },
