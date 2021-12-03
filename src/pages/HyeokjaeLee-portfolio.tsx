afa02f2483f69cdc4fc006a47590ea2a0933fe96
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-12-03 17:57:02 +0900

    💄 refactor(about-page): revise contents
    * unified with the portfolio page
    * improve mobile view
    * change subColor sass function

--- src/pages/HyeokjaeLee-portfolio.tsx
+++ src/pages/HyeokjaeLee-portfolio.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "gatsby";
 import Gmail from "img/gmail.svg";
 import Github from "img/github.svg";
@@ -19,8 +19,6 @@ import Express from "img/express.svg";
 import GraphQL from "img/graphql.svg";
 import Naver from "img/naver.svg";
 import Chrome from "img/chrome.svg";
-import { useContext } from "react";
-import { PortfolioContext } from "contexts/theme";
 
 const React_svg = () => (
   <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -31,7 +29,7 @@ const React_svg = () => (
 const About = () => {
   return (
     <section className="content" style={{ marginTop: "5em" }}>
-      <article className="portfolio">
+      <article className="about">
         <section>
           <h2>💡 About Me</h2>
           <div className="content-wrap">
@@ -90,19 +88,17 @@ const About = () => {
               <p>EDP Operations</p>
               <p>2019.08 ~ </p>
             </div>
-            <div>
-              <ul>
-                <li>배달파트너 배정취소 기능의 개발 지표가 되는 데이터 수집 솔루션 제시/개발</li>
-                <li>지역별 배달 엣지 케이스 딥다이브 과정에 필요한 데이터 클렌징/시각화</li>
-                <li>고객 경험 Worst case 배달파트너 계정 정지 자동화 프로세스 개발</li>
-                <li>업무에 필요한 자동화 스크립트 & 매크로 개발</li>
-                <li>배달파트너 센터 챗봇 개발</li>
-                <li>
-                  식사시간과 서비스 피크타임이 겹쳐 식사 중 발생하는 이슈 트래킹을 위해 노트북을
-                  소지해야 하는 불편함 해소를 위한 봇 개발
-                </li>
-              </ul>
-            </div>
+            <ul className="content-list">
+              <li>배달파트너 배정취소 기능의 개발 지표가 되는 데이터 수집 솔루션 제시/개발</li>
+              <li>지역별 배달 엣지 케이스 딥다이브 과정에 필요한 데이터 클렌징/시각화</li>
+              <li>고객 경험 Worst case 배달파트너 계정 정지 자동화 프로세스 개발</li>
+              <li>업무에 필요한 자동화 스크립트 & 매크로 개발</li>
+              <li>배달파트너 센터 챗봇 개발</li>
+              <li>
+                식사시간과 서비스 피크타임이 겹쳐 식사 중 발생하는 이슈 트래킹을 위해 노트북을
+                소지해야 하는 불편함 해소를 위한 봇 개발
+              </li>
+            </ul>
           </div>
         </section>
         <section className="about-article languages-tools-wrap">
@@ -183,22 +179,22 @@ const About = () => {
                 직관적이고 정형화된 Git Commit message 작성 도우미
                 <p className="project-link">
                   <Github />
-                  <span>Github/gititle -</span>
+                  <span>Github/gititle</span>
                   <a href="https://github.com/HyeokjaeLee/gititle">
                     https://github.com/HyeokjaeLee/gititle
                   </a>
                 </p>
                 <p className="project-link">
                   <Chrome />
-                  <span>Blog Post -</span>
+                  <span>Blog Post</span>
                   <Link to="/gititle-project">https://hyeokjaelee.github.io/gititle-project</Link>
                 </p>
               </div>
-              <ul>
+              <ul className="content-list">
                 <li>
                   커밋 규칙 정리
                   <Link to="/good-commit-message">
-                    (https://hyeokjaelee.github.io/good-commit-message)
+                    https://hyeokjaelee.github.io/good-commit-message
                   </Link>
                 </li>
                 <li>커밋에 사용할 키워드 데이터 구조 정의</li>
@@ -223,20 +219,20 @@ const About = () => {
                 공주대학교 비대면 강의 시스템 편의성 개선 프로그램
                 <p className="project-link">
                   <Github />
-                  <span>Github/knu-lms-scheduler -</span>
+                  <span>Github/knu-lms-scheduler</span>
                   <a href="https://github.com/HyeokjaeLee/knu-lms-scheduler">
                     https://github.com/HyeokjaeLee/knu-lms-scheduler
                   </a>
                 </p>
                 <p className="project-link">
                   <Chrome />
-                  <span>Blog Post -</span>
+                  <span>Blog Post</span>
                   <Link to="/knu-lms-scheduler">
                     https://hyeokjaelee.github.io/knu-lms-scheduler
                   </Link>
                 </p>
               </div>
-              <ul>
+              <ul className="content-list">
                 <li>Electron 백엔드, 비대면 강의 시스템 로그인 크롤러 개발</li>
                 <li>Electron 프론트엔드, React 디자인/개발</li>
                 <li>프로그램 홍보, 배포, 피드백 반영 업데이트</li>
@@ -263,27 +259,27 @@ const About = () => {
                 여러 플랫폼의 웹툰을 한번에 볼 수 있는 웹 애플리케이션
                 <p className="project-link">
                   <Github />
-                  <span>Github/korea-webtoon-api -</span>
+                  <span>Github/korea-webtoon-api</span>
                   <a href="https://github.com/HyeokjaeLee/korea-webtoon-api">
                     https://github.com/HyeokjaeLee/korea-webtoon-api
                   </a>
                 </p>
                 <p className="project-link">
                   <Github />
-                  <span>Github/webtoon-hub -</span>
+                  <span>Github/webtoon-hub</span>
                   <a href="https://github.com/HyeokjaeLee/webtoon-hub">
                     https://github.com/HyeokjaeLee/webtoon-hub
                   </a>
                 </p>
                 <p className="project-link">
                   <Chrome />
-                  <span>Blog Post -</span>
+                  <span>Blog Post</span>
                   <Link to="/korea-webtoon-api-update">
                     https://hyeokjaelee.github.io/korea-webtoon-api-update
                   </Link>
                 </p>
               </div>
-              <ul>
+              <ul className="content-list">
                 <li>NestJS 서버 구축</li>
                 <li>각 플랫폼별 크롤러 개발</li>
                 <li>플랫폼별 통합 웹툰 데이터 구조 정의</li>
@@ -311,20 +307,20 @@ const About = () => {
                 여러 플랫폼의 웹툰을 한번에 볼 수 있는 웹 애플리케이션
                 <p className="project-link">
                   <Github />
-                  <span>Github/korea-covid19-api -</span>
+                  <span>Github/korea-covid19-api</span>
                   <a href="https://github.com/HyeokjaeLee/korea-covid19-api">
                     https://github.com/HyeokjaeLee/korea-covid19-api
                   </a>
                 </p>
                 <p className="project-link">
                   <Github />
-                  <span>Github/covid19-dashboard -</span>
+                  <span>Github/covid19-dashboard</span>
                   <a href="https://github.com/HyeokjaeLee/covid19-dashboard">
                     https://github.com/HyeokjaeLee/covid19-dashboard
                   </a>
                 </p>
               </div>
-              <ul>
+              <ul className="content-list">
                 <li>Express 서버 구축</li>
                 <li>COVID-19 데이터 정형화 & 계산을 통한 확장 값 추가</li>
                 <li>공공데이터 포털의 COVID-19 데이터 이상치 제거</li>
@@ -351,20 +347,20 @@ const About = () => {
                 개인 블로그
                 <p className="project-link">
                   <Github />
-                  <span>Github/blog -</span>
+                  <span>Github/blog</span>
                   <a href="https://github.com/HyeokjaeLee/hyeokjaelee.github.io">
                     https://github.com/HyeokjaeLee/hyeokjaelee.github.io
                   </a>
                 </p>
                 <p className="project-link">
                   <Chrome />
-                  <span>Blog Post -</span>
+                  <span>Blog Post</span>
                   <Link to="/blog-remake-review1">
                     https://hyeokjaelee.github.io/korea-webtoon-api-update
                   </Link>
                 </p>
               </div>
-              <ul>
+              <ul className="content-list">
                 <li>프론트엔드 Gatsby(React) 디자인/개발</li>
                 <li>템플릿 공개</li>
               </ul>
@@ -381,12 +377,15 @@ const About = () => {
               <p>2016.03 ~ 졸업예정</p>
             </div>
             <div>
-              <ul>
+              <ul className="content-list">
                 <li>
-                  <a href="https://namu.wiki/w/%EA%B3%B5%EC%A3%BC%EB%8C%80%ED%95%99%EA%B5%90/%ED%95%99%EB%B6%80/%EA%B3%B5%EA%B3%BC%EB%8C%80%ED%95%99?rev=316#s-2.3.4">
+                  <a
+                    style={{ marginLeft: "0" }}
+                    href="https://namu.wiki/w/%EA%B3%B5%EC%A3%BC%EB%8C%80%ED%95%99%EA%B5%90/%ED%95%99%EB%B6%80/%EA%B3%B5%EA%B3%BC%EB%8C%80%ED%95%99?rev=316#s-2.3.4"
+                  >
                     PRIMITVE
                   </a>
-                  /개발 & 창업 동아리 활동 (2016 ~ )
+                  개발 & 창업 동아리 활동 (2016 ~ )
                 </li>
                 <li>정보처리기사 취득 (2021.6)</li>
               </ul>
