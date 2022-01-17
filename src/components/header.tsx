0a37138ab940ce8e6894dc2730a05a9ae2b41ab9
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2022-01-17 20:00:14 +0900

    🚧 refactor: apply css module

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import logo from "img/logo.png";
 import * as style from "styles/header.module.scss";
 export const Header = () => {
+  const theme = "";
   return (
     <header className={style.header}>
-      <img src={logo} className={style.logo} alt="title-logo"></img>
+      <img src={logo} className={style.logo + theme} alt="title-logo"></img>
       <span className="title">
         <h1>Nagle's blog</h1>
         <p>Programming & Tech</p>
