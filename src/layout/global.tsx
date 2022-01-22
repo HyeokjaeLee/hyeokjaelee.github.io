703e08c2eef7dffa262fb903c9846dfe7ba3e9db
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2022-01-22 23:42:59 +0900

    🚧 refactor: improve navbar interface

--- src/layout/global.tsx
+++ src/layout/global.tsx
@@ -1,6 +1,5 @@
-import "styles/index.scss";
 import "styles/global.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import { Helmet } from "react-helmet";
 import { Main } from "components/main";
@@ -10,17 +9,14 @@ import { Footer } from "components/footer";
 import { PortfolioLink } from "components/portfolio-link";
 import { ThemeContext, PortfolioContext } from "contexts/theme";
 import MenuIcon from "img/menu.svg";
-import * as style from "styles/header.module.scss";
 import logo from "img/logo.png";
 import favicon from "assets/img/favicon.ico";
-import { Navbar } from "components/navbar";
 
 const TemplateWrapper = ({ children, location }: any) => {
   const [theme, setTheme] = useState("");
   const [portfolio, setPortfolio]: [Display, SetDisplay] = useState("hide");
   const [menuShow, setMenuShow] = useState(false);
   const setMenuClass = (menuShow: boolean) => (menuShow ? "show" : "hide");
-  const _Header = location.pathname.includes("HyeokjaeLee") ? <></> : <Header />;
   const helmet_meta_otions = [
     { name: `description`, content: `programming & tech blog` },
     { name: `generator`, content: `gatsby` },
@@ -30,17 +26,16 @@ const TemplateWrapper = ({ children, location }: any) => {
       content: "6KHb8V0fAsVjI-k0fZfovmdLDrfikkgwpVMCsfSmvrw",
     },
   ];
+
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <PortfolioContext.Provider value={{ portfolio, setPortfolio }}>
         <Helmet title="Nagle`s Blog" meta={helmet_meta_otions} bodyAttributes={{ class: theme }}>
           <link rel="icon" href={favicon} />
         </Helmet>
-        <header id="header">
-          <Navbar />
-        </header>
-        <Main location={location}>{children}</Main>
-        <PortfolioLink />
+        <Header />
+        <div className="test2"></div>
+        <div className="test"></div>
       </PortfolioContext.Provider>
     </ThemeContext.Provider>
   );
@@ -53,4 +48,18 @@ export default TemplateWrapper;
           <button id="nav-menu-button">
             <MenuIcon className="menu-icon" />
           </button>
+
+
+              <ThemeContext.Provider value={{ theme, setTheme }}>
+      <PortfolioContext.Provider value={{ portfolio, setPortfolio }}>
+        <Helmet title="Nagle`s Blog" meta={helmet_meta_otions} bodyAttributes={{ class: theme }}>
+          <link rel="icon" href={favicon} />
+        </Helmet>
+        <header id="header">
+          <Navbar />
+        </header>
+        <Main location={location}>{children}</Main>
+        <PortfolioLink />
+      </PortfolioContext.Provider>
+    </ThemeContext.Provider>
  */
