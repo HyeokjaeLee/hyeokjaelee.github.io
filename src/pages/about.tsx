37b44033c0a54f1a0c047140002e782b7393c009
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2022-01-30 00:36:51 +0900

    💄 refactor(about): devide-section style class 통합
    * 비슷한 레이아웃을 가진 calss들 호환되도록 통합
    * 일부 UI 변경
    * react svg 파일 추가

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -22,16 +22,12 @@ import Puppeteer from "img/puppeteer.svg";
 import MongoDB from "img/mongodb.svg";
 import SQLite from "img/sqlite.svg";
 import Jest from "img/jest.svg";
+import React_svg from "img/reactIcon.svg";
 import { useContext } from "react";
 import { PortfolioContext, ThemeContext } from "contexts/theme";
 import * as style from "styles/pages/about.module.scss";
-const React_svg = () => (
-  <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <title>React</title>
-    <path d="M14.23 12.004a2.236 2.236 0 0 1-2.235 2.236 2.236 2.236 0 0 1-2.236-2.236 2.236 2.236 0 0 1 2.235-2.236 2.236 2.236 0 0 1 2.236 2.236zm2.648-10.69c-1.346 0-3.107.96-4.888 2.622-1.78-1.653-3.542-2.602-4.887-2.602-.41 0-.783.093-1.106.278-1.375.793-1.683 3.264-.973 6.365C1.98 8.917 0 10.42 0 12.004c0 1.59 1.99 3.097 5.043 4.03-.704 3.113-.39 5.588.988 6.38.32.187.69.275 1.102.275 1.345 0 3.107-.96 4.888-2.624 1.78 1.654 3.542 2.603 4.887 2.603.41 0 .783-.09 1.106-.275 1.374-.792 1.683-3.263.973-6.365C22.02 15.096 24 13.59 24 12.004c0-1.59-1.99-3.097-5.043-4.032.704-3.11.39-5.587-.988-6.38-.318-.184-.688-.277-1.092-.278zm-.005 1.09v.006c.225 0 .406.044.558.127.666.382.955 1.835.73 3.704-.054.46-.142.945-.25 1.44-.96-.236-2.006-.417-3.107-.534-.66-.905-1.345-1.727-2.035-2.447 1.592-1.48 3.087-2.292 4.105-2.295zm-9.77.02c1.012 0 2.514.808 4.11 2.28-.686.72-1.37 1.537-2.02 2.442-1.107.117-2.154.298-3.113.538-.112-.49-.195-.964-.254-1.42-.23-1.868.054-3.32.714-3.707.19-.09.4-.127.563-.132zm4.882 3.05c.455.468.91.992 1.36 1.564-.44-.02-.89-.034-1.345-.034-.46 0-.915.01-1.36.034.44-.572.895-1.096 1.345-1.565zM12 8.1c.74 0 1.477.034 2.202.093.406.582.802 1.203 1.183 1.86.372.64.71 1.29 1.018 1.946-.308.655-.646 1.31-1.013 1.95-.38.66-.773 1.288-1.18 1.87-.728.063-1.466.098-2.21.098-.74 0-1.477-.035-2.202-.093-.406-.582-.802-1.204-1.183-1.86-.372-.64-.71-1.29-1.018-1.946.303-.657.646-1.313 1.013-1.954.38-.66.773-1.286 1.18-1.868.728-.064 1.466-.098 2.21-.098zm-3.635.254c-.24.377-.48.763-.704 1.16-.225.39-.435.782-.635 1.174-.265-.656-.49-1.31-.676-1.947.64-.15 1.315-.283 2.015-.386zm7.26 0c.695.103 1.365.23 2.006.387-.18.632-.405 1.282-.66 1.933-.2-.39-.41-.783-.64-1.174-.225-.392-.465-.774-.705-1.146zm3.063.675c.484.15.944.317 1.375.498 1.732.74 2.852 1.708 2.852 2.476-.005.768-1.125 1.74-2.857 2.475-.42.18-.88.342-1.355.493-.28-.958-.646-1.956-1.1-2.98.45-1.017.81-2.01 1.085-2.964zm-13.395.004c.278.96.645 1.957 1.1 2.98-.45 1.017-.812 2.01-1.086 2.964-.484-.15-.944-.318-1.37-.5-1.732-.737-2.852-1.706-2.852-2.474 0-.768 1.12-1.742 2.852-2.476.42-.18.88-.342 1.356-.494zm11.678 4.28c.265.657.49 1.312.676 1.948-.64.157-1.316.29-2.016.39.24-.375.48-.762.705-1.158.225-.39.435-.788.636-1.18zm-9.945.02c.2.392.41.783.64 1.175.23.39.465.772.705 1.143-.695-.102-1.365-.23-2.006-.386.18-.63.406-1.282.66-1.933zM17.92 16.32c.112.493.2.968.254 1.423.23 1.868-.054 3.32-.714 3.708-.147.09-.338.128-.563.128-1.012 0-2.514-.807-4.11-2.28.686-.72 1.37-1.536 2.02-2.44 1.107-.118 2.154-.3 3.113-.54zm-11.83.01c.96.234 2.006.415 3.107.532.66.905 1.345 1.727 2.035 2.446-1.595 1.483-3.092 2.295-4.11 2.295-.22-.005-.406-.05-.553-.132-.666-.38-.955-1.834-.73-3.703.054-.46.142-.944.25-1.438zm4.56.64c.44.02.89.034 1.345.034.46 0 .915-.01 1.36-.034-.44.572-.895 1.095-1.345 1.565-.455-.47-.91-.993-1.36-1.565z" />
-  </svg>
-);
 
+const EMPTY_ELEMENT = (props: any) => <></>;
 interface ProjectInfo {
   stackList: any[];
   titleUnderInfoList: string[];
@@ -40,7 +36,20 @@ interface ProjectInfo {
   additionalInfoList: string[];
 }
 
-const About = ({ location }: any) => {
+function Project({
+  title,
+  stackList,
+  description,
+  linkMap,
+  additionalInfoList,
+}: {
+  title: string;
+  stackList: string[];
+  description: string;
+  linkMap: Map<string, string>;
+  additionalInfoList: string[];
+}) {}
+export default function About({ location }: any) {
   const { search }: { search: string } = location;
   const { setPortfolioOptions } = useContext(PortfolioContext);
   const { theme } = useContext(ThemeContext);
@@ -49,7 +58,7 @@ const About = ({ location }: any) => {
   useEffect(() => {
     isPortfolio && setPortfolioOptions({ isPortfolio: true, portfolioButtonShow: false });
   }, []);
-  const EMPTY_ELEMENT = () => <></>;
+
   let ProjectListElement = EMPTY_ELEMENT;
   let WorkExperienceElement = EMPTY_ELEMENT;
   if (isPortfolio) {
@@ -198,25 +207,28 @@ const About = ({ location }: any) => {
     WorkExperienceElement = () => (
       <section>
         <h2>👨🏻‍💻 Work Experience</h2>
-        <div className={`${style.sectionFlex} ${style.sectionMargin}`}>
-          <h3>Coupang Eats</h3>
-          <p>2019년 5월부터 서비스 중인 30분 로켓 음식 배달 서비스</p>
-          <div className={style.devidedSection}>
-            <div className={style.sectionTitle}>
-              <p>EDP Operations</p>
-              <p>2019.08 ~ 2020.02</p>
-              <span className={style.miniTxt}>6개월</span>
-            </div>
-            <ul className={style.sectionDescription}>
+        <div className={`${style.devidedSection} ${style.sectionMargin}`}>
+          <div className={style.sectionTitle}>
+            <h3>Coupang Eats</h3>
+            <ul className={style.titleUnderInfoList}>
+              <li>EDP Operations</li>
               <li>
-                배달파트너 배정취소 기능의 개발 지표가 되는 데이터 수집 솔루션 제시/개발&nbsp;
+                2019.08 ~ 2020.02 <span className={style.miniTxt}>6개월</span>
+              </li>
+            </ul>
+          </div>
+          <div className={style.sectionDescription}>
+            <h4>2019년 5월부터 서비스 중인 30분 로켓 음식 배달 서비스</h4>
+            <ul>
+              <li>
+                배달파트너 배정취소 기능의 개발 지표가 되는 데이터 수집 솔루션 제시/개발
                 <span className={style.miniTxt}>담당 인원 75% 절감</span>
               </li>
               <li>지역별 배달 엣지 케이스 딥다이브 과정에 필요한 데이터 클렌징/시각화</li>
               <li>고객 경험 Worst case 배달파트너 계정 정지 자동화 프로세스 개발</li>
               <li>
-                업무에 필요한 자동화 스크립트 & 매크로 개발 &nbsp;
-                <span className={style.miniTxt}>업무당 담당 인원 한 명 수준으로 절감</span>
+                업무에 필요한 자동화 스크립트 & 매크로 개발
+                <span className={style.miniTxt}>담당 인원 한 명 수준으로 절감</span>
               </li>
               <li>배달파트너 센터 챗봇 개발</li>
               <li>
@@ -354,6 +366,4 @@ const About = ({ location }: any) => {
       )}
     </article>
   );
-};
-
-export default About;
+}
