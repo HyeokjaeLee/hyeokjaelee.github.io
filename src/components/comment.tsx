643ea40f19ebccfef392ea90f6fe249101d34143
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2022-02-05 20:13:32 +0900

    🩹 fix: 개발환경 utteranc 버그 수정
    * comment 컴포넌트 복잡도 개선
    * utteranc ref 대상 element 컴포넌트화를 통해 버그 수정
    
    close #25

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -1,34 +1,25 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { ThemeContext } from "contexts/theme";
+const attributes = new Map([
+  ["src", "https://utteranc.es/client.js"],
+  ["repo", "HyeokjaeLee/hyeokjaelee.github.io"],
+  ["issue-term", "url"],
+  ["crossorigin", "anonymous"],
+  ["async", "false"],
+]);
 export const Comment = () => {
-  //적당한 타입을 못찾겠음.
-  const comment: any = useRef();
-  const [status, setStatus] = useState("pending");
+  const comment = useRef<HTMLElement>(null);
+  const CommentWrap = () => <section ref={comment} />;
   const { theme } = useContext(ThemeContext);
   useEffect(() => {
-    const commentElement: HTMLElement = comment.current;
+    const commentElement = comment.current;
+    if (!commentElement) return;
     const commentScript = document.createElement("script");
-    const attributes = {
-      src: `https://utteranc.es/client.js`,
-      repo: "HyeokjaeLee/hyeokjaelee.github.io",
-      "issue-term": "url",
-      theme: theme === "dark" ? "photon-dark" : "github-light",
-      crossorigin: "anonymous",
-      async: "true",
-    };
-    Object.entries(attributes).forEach(([key, value]) => {
-      commentScript.setAttribute(key, value);
-    });
-    commentScript.onload = () => setStatus("success");
-    commentScript.onerror = () => setStatus("failed");
+    for (const [attribute, value] of attributes) commentScript.setAttribute(attribute, value);
+    commentScript.setAttribute("theme", theme === "dark" ? "photon-dark" : "github-light");
     commentElement.innerHTML = "";
     commentElement.appendChild(commentScript);
   }, [theme]);
-  return (
-    <div className="content">
-      <section ref={comment} />
-      {status === "failed" && <div>Error. Please try again.</div>}
-      {status === "pending" && <div>Loading script...</div>}
-    </div>
-  );
+
+  return <CommentWrap />;
 };
