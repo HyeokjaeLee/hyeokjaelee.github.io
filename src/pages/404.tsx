27657b308a80ec14f74c4c14fad8ede46db1538a
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2022-02-13 21:35:08 +0900

    💄 refactor(404):  페이지 개선

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,19 @@
+import { Link } from "gatsby";
 import React from "react";
-const NotFoundPage = () => <></>;
+import * as style from "styles/pages/404.module.scss";
+const NotFoundPage = () => {
+  return (
+    <article className={style.notFound}>
+      <div className={style.unhappyFace}>:(</div>
+      <h1>404 ERROR</h1>
+      <h3>페이지를 찾을 수 없습니다.</h3>
+      <p className={style.marginTop}>페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.</p>
+      <p>입력하신 주소가 정확한지 다시 한 번 확인해주세요.</p>
+      <Link to="/" className={style.goHome}>
+        HOME
+      </Link>
+    </article>
+  );
+};
 
 export default NotFoundPage;
