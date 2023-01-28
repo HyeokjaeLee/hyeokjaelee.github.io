1b43733f90c0d37c43c02b73b66e269e7a5f1f4c
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-01-29 02:56:57 +0900

    chore: gatsby 초기 설정 (#43)
    * chore: gatsby blog 초기 설정
    
    * chore: .nvmrc 파일 추가
    
    - node version 18.13.0 (LTS)
    
    * chore: package 의존성 업데이트
    
    * chore: package manager 변경
    
    - npm > yarn berry (zero-install)
    
    * chore: typescript 환경 설정
    
    * chore: sass 설정
    
    - 누락된 typescript 설정 추가

---
+++ src/pages/404.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import type { PageProps } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+interface DataProps {
+  site: {
+    siteMetadata: {
+      title: string
+    }
+  }
+}
+
+const NotFoundPage = ({ data, location }: PageProps<DataProps>) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <Seo title="404: Not Found" />
+
+export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
