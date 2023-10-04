6c8db32a5aa8ebc4f643bdf9b384370e01d77465
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-10-04 13:43:06 +0900

    feat: PostCardImage 컴포넌트 제거

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { Tag } from 'react-feather';
 
 import type { PostData } from '@hooks/useGetPostDataList';
-import { Tooltip } from '@hyeokjaelee/pastime-ui';
+import { Skeleton, Tooltip } from '@hyeokjaelee/pastime-ui';
 import { Link } from '@reach/router';
-
-import { PostCardImage } from './PostCardImage';
 
 type PostCardProps = PostData;
 
@@ -20,7 +18,15 @@ export const PostCard = ({
   <li key={slug} className="w-full md:w-1/2 lg:w-1/4">
     <Link to={slug} className="p-2 block hover:pt-0 hover:pb-4 transition-all">
       <article className="w-full transition-shadow rounded-md hover:shadow-lg dark:shadow-black">
-        <PostCardImage src={titleImage} alt={title} />
+        <div className="relative w-full h-0 pb-[50%] rounded-t-md overflow-hidden">
+          <Skeleton className="absolute w-full h-full object-cover" />
+          <img
+            src={titleImage}
+            alt={title}
+            loading="lazy"
+            className="absolute w-full h-full object-cover"
+          />
+        </div>
         <section className="bg-white dark:bg-zinc-700 p-3 flex flex-col">
           <h4 className="truncate">{title}</h4>
           <div className="h-[3.75em]">
