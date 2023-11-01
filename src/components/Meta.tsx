27ca666f1c48d97c89921884da07d57153899c5c
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-11-01 14:51:33 +0900

    chore: google analytics 설정 (#86)
    * chore: google analytics 태그 삽입
    
    * refactor(PostCard): 이미지 로딩 개선
    
    * fix(SkillItem): key prop 추가
    
    - StackBadge 이미지 최적화
    
    * ver: 1.0.4

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -2,6 +2,22 @@ import React from 'react';
 
 import { useGetSiteMetaData } from '@hooks/useGetSiteMetaData';
 
+const GoogleAnalytics = () => (
+  <>
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-DEXMW0T0W6"
+    />
+    <script>
+      {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+      
+          gtag('config', 'G-DEXMW0T0W6');`}
+    </script>
+  </>
+);
+
 interface MetaProps {
   title?: string | null;
   description?: string | null;
@@ -30,6 +46,7 @@ export const Meta = ({ title, description, children }: MetaProps) => {
       <meta name="twitter:title" content={metaTitle} />
       <meta name="twitter:description" content={metaDescription} />
       <meta name="theme-color" content="#000000" />
+      <GoogleAnalytics />
       {children}
     </>
   );
