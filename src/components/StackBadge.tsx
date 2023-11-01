e61e872abaa3b2fe7e0262911e104f6949c7af4c
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-11-01 15:23:08 +0900

    hotfix: 이미지 최적화 롤백

--- src/components/StackBadge.tsx
+++ src/components/StackBadge.tsx
@@ -14,24 +14,15 @@ export const StackBadge = ({
   backgroundColor,
   blackLogo,
   logo,
-}: StackBadgeProps) => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  return (
-    <>
-      <Skeleton className={`h-5 w-12 ${isLoading ? '' : 'hidden'}`} />
-      <img
-        className={`${isLoading ? 'hidden' : ''}`}
-        src={`https://img.shields.io/badge/${name}-${backgroundColor.replace(
-          '#',
-          '',
-        )}?style=flat-square&logo=${logo ?? name}&logoColor=${
-          blackLogo ? 'black' : 'white'
-        }`}
-        alt={name}
-        loading="lazy"
-        onLoad={() => setIsLoading(false)}
-      />
-    </>
-  );
-};
+}: StackBadgeProps) => (
+  <img
+    src={`https://img.shields.io/badge/${name}-${backgroundColor.replace(
+      '#',
+      '',
+    )}?style=flat-square&logo=${logo ?? name}&logoColor=${
+      blackLogo ? 'black' : 'white'
+    }`}
+    alt={name}
+    loading="lazy"
+  />
+);
