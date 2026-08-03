ce0000524c670c43b8e8572d86f5ead9e00f26c4
HyeokjaeLee <leehyeokjae97@gmail.com>
2026-08-04 02:33:12 +0900

    feat: 메뉴 사이드바 열고 닫기 슬라이드 애니메이션 추가
    - DrawerContent에 상하좌우 방향별 slide-in/slide-out 애니메이션 클래스 추가

--- src/components/molecules/Drawer.tsx
+++ src/components/molecules/Drawer.tsx
@@ -54,6 +54,11 @@ function DrawerContent({
         data-slot="drawer-content"
         className={cn(
           'group/drawer-content bg-background z-51 fixed flex h-auto flex-col',
+          'data-[state=open]:animate-in data-[state=closed]:animate-out',
+          'data-[vaul-drawer-direction=top]:data-[state=open]:slide-in-from-top data-[vaul-drawer-direction=top]:data-[state=closed]:slide-out-to-top',
+          'data-[vaul-drawer-direction=bottom]:data-[state=open]:slide-in-from-bottom data-[vaul-drawer-direction=bottom]:data-[state=closed]:slide-out-to-bottom',
+          'data-[vaul-drawer-direction=left]:data-[state=open]:slide-in-from-left data-[vaul-drawer-direction=left]:data-[state=closed]:slide-out-to-left',
+          'data-[vaul-drawer-direction=right]:data-[state=open]:slide-in-from-right data-[vaul-drawer-direction=right]:data-[state=closed]:slide-out-to-right',
           'data-[vaul-drawer-direction=top]:inset-x-0 data-[vaul-drawer-direction=top]:top-0 data-[vaul-drawer-direction=top]:mb-24 data-[vaul-drawer-direction=top]:max-h-[80vh] data-[vaul-drawer-direction=top]:rounded-b-lg data-[vaul-drawer-direction=top]:border-b',
           'data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:mt-24 data-[vaul-drawer-direction=bottom]:max-h-[80vh] data-[vaul-drawer-direction=bottom]:rounded-t-lg data-[vaul-drawer-direction=bottom]:border-t',
           'data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:right-0 data-[vaul-drawer-direction=right]:w-3/4 data-[vaul-drawer-direction=right]:border-l data-[vaul-drawer-direction=right]:sm:max-w-sm',
